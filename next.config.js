c9670787b64a3f6148e15db39ad3b839388a31f5
Grant Weston <your.email@example.com>
2024-12-07 15:54:28 -0600

    fix: set explicit frame options to allow embedding

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  security: {
-    dangerouslyDisableDefaultSecurity: true
-  },
   async headers() {
     return [
       {
@@ -27,8 +24,12 @@ const nextConfig = {
             value: 'true'
           },
           {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          },
+          {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' https://*.live.com https://*.microsoft.com https://*.office.com https://*.office365.com; frame-ancestors 'self' https://*.live.com https://*.outlook.com https://*.office.com https://*.office365.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.microsoft.com https://*.office.com; connect-src 'self' https://*.microsoft.com https://*.office.net https://*.office.com https://*.live.com"
+            value: "frame-ancestors *; default-src 'self' https://*.live.com https://*.microsoft.com https://*.office.com https://*.office365.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.microsoft.com https://*.office.com; connect-src 'self' https://*.microsoft.com https://*.office.net https://*.office.com https://*.live.com"
           }
         ],
       }
