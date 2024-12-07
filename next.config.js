6608e14e74c951c7fbeef537cc52995c6ba39827
Grant Weston <your.email@example.com>
2024-12-07 15:59:04 -0600

    fix: consolidate headers in vercel.json

--- next.config.js
+++ next.config.js
@@ -1,40 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*'
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, OPTIONS'
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: '*'
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL'
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors *; default-src 'self' https://*.live.com https://*.microsoft.com https://*.office.com https://*.office365.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.microsoft.com https://*.office.com; connect-src 'self' https://*.microsoft.com https://*.office.net https://*.office.com https://*.live.com"
-          }
-        ],
-      }
-    ]
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
