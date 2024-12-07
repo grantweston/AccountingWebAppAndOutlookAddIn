047164f5da5dcdfdee6bbd37d8510a70ead68a23
Grant Weston <your.email@example.com>
2024-12-07 15:27:54 -0600

    fix: complete CORS and CSP headers for Outlook integration

--- next.config.js
+++ next.config.js
@@ -20,8 +20,12 @@ const nextConfig = {
             value: '*'
           },
           {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true'
+          },
+          {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://outlook.live.com https://*.outlook.com https://*.outlook.office.com https://*.outlook.office365.com; connect-src 'self' https://*.microsoft.com https://*.office.net https://*.office.com"
+            value: "frame-ancestors 'self' https://outlook.live.com https://*.outlook.com https://*.outlook.office.com https://*.outlook.office365.com; connect-src 'self' https://*.microsoft.com https://*.office.net https://*.office.com https://*.live.com"
           }
         ],
       },
